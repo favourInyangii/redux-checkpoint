b3b05a5ece407bd576073257db2f69179e0ab281
favour ololade <ololadefavourajayi@gmail.com>
2024-06-21 13:06:35 +0100

    app.js modification

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import AddTask from './components/AddTask';
+import ListTask from './components/ListTask';
+import './styles.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className="App">
+            <h1>To-Do List with Redux</h1>
+            <AddTask />
+            <ListTask />
+        </div>
+    );
+};
 
 export default App;
